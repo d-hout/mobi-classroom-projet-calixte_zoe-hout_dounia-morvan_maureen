f8a1f4c5f6e3de88a12f8e222d83cd32eedd6bc6
Maureen <maumorvan@ensc.fr>
2026-03-20 14:36:05 +0100

    Creation du projet

---
+++ projet_mobi/src/contexte/counterContext.js
@@ -0,0 +1,18 @@
+import { createContext } from 'react';
+
+export const CounterContext = createContext({ 
+    counter: 0, 
+    increment(){}
+});
+
+export default function CounterContextProvider({ children }) {
+  const [counter, setCounter] = useState(0);
+  return (<CounterContext.Provider value={{
+      counter,
+      increment(){
+          setCounter(counter + 1);
+      }
+  }}>
+      { children }
+  </CounterContext.Provider>);
+}

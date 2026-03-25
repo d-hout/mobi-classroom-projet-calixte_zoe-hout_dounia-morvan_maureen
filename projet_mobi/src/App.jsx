1918f02cb2663108aeebd870fcaceafbbb147f21
d-hout <dhout@ensc.fr>
2026-03-25 17:23:12 +0100

    test connexion à l'api

--- projet_mobi/src/App.jsx
+++ projet_mobi/src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { observeAuth } from "./services/authService";
 import LoginPage from "./pages/LoginPage";
-import './App.css'
-import CounterContextProvider from './assets/counterContext'
-import Child from './Child'
-import Header from './components/Header';
+import "./App.css";
+import CounterContextProvider from "./assets/counterContext";
+import Child from "./Child";
+import Header from "./components/Header";
+import DisneyTest from "./components/DisneyTest";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -14,30 +15,33 @@ function App() {
     const unsubscribe = observeAuth((firebaseUser) => {
       setUser(firebaseUser || null);
     });
- 
+
     return () => unsubscribe();
   }, []);
- 
+
   if (user === undefined) {
     return <p>Chargement...</p>;
   }
- 
+
   if (!user) {
     return <LoginPage />;
   }
 
   return (
-    <>    
+    <>
       <CounterContextProvider>
-          <div>
-            <Header/>
-            <h1>Bienvenue {user.displayName}</h1>
-            <p>{user.email}</p>
-            <Child/>
-          </div>
+        <div>
+          <Header />
+          <h1>Bienvenue {user.displayName}</h1>
+          <p>{user.email}</p>
+          <Child />
+          <hr />
+          <h2>Test API Disney</h2>
+          <DisneyTest />
+        </div>
       </CounterContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

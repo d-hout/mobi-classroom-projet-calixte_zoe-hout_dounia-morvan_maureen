12c8934c1ea0ba2f137c3492883acccebe36f66d
zcalixte <zcalixte@ensc.fr>
2026-03-26 21:45:09 +0100

    Modifications page accueil

--- projet_mobi/src/pages/LoginPage.jsx
+++ projet_mobi/src/pages/LoginPage.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { loginWithGoogle } from "../services/authService";
 import googleLogo from "../assets/logo-google.png";
 import "./loginPage.css";
-import spidermanImg from "../assets/spiderman.png";
-import captain_marvelImg from "../assets/captain_marvel.png";
+import minnieImg from "../assets/minie.jpg";
+import simbaImg from "../assets/simba.jpg";
+
 
 function LoginPage() {
   const [loading, setLoading] = useState(false);
@@ -31,14 +32,14 @@ function LoginPage() {
             rel="stylesheet"
           ></link>
           <h2>
-            Bienvenue dans Marvel card Arena
+            Bienvenue dans l'univers Disney 
             <br />
-            <span> Crée ton deck pour remporter la partie</span>
+            <span> Crée ton deck pour remporter la partie </span>
           </h2>
 
           <p>
-            Un jeu de cartes immersif dans l'univers Marvel
-            <br />
+            Un jeu de cartes immersif 
+            <br/>
           </p>
 
           <div className="hero-actions">
@@ -61,23 +62,23 @@ function LoginPage() {
             <div className="bg-circle yellow" />
             <div className="bg-circle green" />
 
-            <div className="mock-card spider">
+            <div className="mock-card minnie">
               <div className="mock-image ">
-                <img src={spidermanImg} alt="Spider-Man" />
+                <img src={minnieImg} alt="Minnie" />
               </div>
-              <h3>Spider-Man</h3>
-              <p>Marvel hero card</p>
+              <h3>Minnie</h3>
+              <p>Disney card</p>
               <div className="stats-row">
                 <span>ATK 7</span>
                 <span>DEF 5</span>
               </div>
             </div>
 
-            <div className="mock-card captain">
+            <div className="mock-card simba">
               <div className="mock-image">
-                <img src={captain_marvelImg} alt="Captain Marvel" />
+                <img src={simbaImg} alt="Simba" />
               </div>
-              <h3>Captain Marvel</h3>
+              <h3>Simba</h3>
               <p>Marvel hero card</p>
               <div className="stats-row">
                 <span>ATK 9</span>
